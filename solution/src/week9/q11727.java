e1155c1422a6bebd010b7043f1bb2bc95a62fde5
realcommang <katlelo@naver.com>
2023-09-13 16:03:15 +0900

    [ADD] 2407

---
+++ solution/src/week9/q11727.java
@@ -0,0 +1,15 @@
+package week9;
+import java.util.*;
+
+public class q11727 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		
+	}
+
+}
---
+++ solution/src/week9/q2407.java
@@ -0,0 +1,32 @@
+package week9;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class q2407 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		BigInteger num1 = BigInteger.ONE;
+		BigInteger num2 = BigInteger.ONE;
+		
+		for(int i = 0; i < m; i++) {
+			num1 = num1.multiply(new BigInteger(String.valueOf(n-i)));
+			num2 = num2.multiply(new BigInteger(String.valueOf(i + 1)));
+		}
+		
+		BigInteger sum = num1.divide(num2);
+		
+		System.out.println(sum);
+		
+		
+	}
+
+}
