08c3c36ce2a5a8461ab5bd80c983eaaccc0ed4c3
realcommang <katlelo@naver.com>
2023-09-21 16:14:19 +0900

    [ADD] 1012

---
+++ solution/src/week10/q1012.java
@@ -0,0 +1,92 @@
+package week10;
+
+import java.util.*;
+import java.io.*;
+
+public class q1012 {
+
+	static int dx[] = {0,0,1,-1};
+	static int dy[] = {1,-1,0,0};
+	static int M, N, K;
+	static int [][] cabbage = new int [51][51]; //양배추 밭
+	static boolean [][] visit = new boolean [51][51];
+	static int bugs; 
+	 
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		ArrayList<Integer> nums = new ArrayList<Integer>();
+		
+		while( T-- > 0) {
+			
+			bugs = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			
+			cabbage = new int [M][N];
+			visit = new boolean [M][N];
+			
+			for(int i = 0; i < M; i++) { //초기화
+				for(int j = 0; j < N; j++) {
+					cabbage[i][j] = 0;
+					visit[i][j] = false;
+				}
+			}
+			
+			for(int i = 0; i < K; i++) {//양배추 위치
+				StringTokenizer str = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(str.nextToken());
+				int y = Integer.parseInt(str.nextToken());
+				cabbage[x][y] = 1;
+			}
+			
+			for(int i = 0; i < M; i++) { //초기화
+				for(int j = 0; j < N; j++) {
+					if(cabbage[i][j] == 1) {
+						countBugs(i, j);
+					}
+				}
+			}
+			
+			//System.out.println(bugs);
+			nums.add(bugs);
+		}
+		
+		for(int i = 0; i < nums.size(); i++) {
+			System.out.println(nums.get(i));
+		}
+	}
+
+	static void countBugs(int i, int j) {	
+		
+		boolean result = true;
+		
+		for(int k = 0; k < 4; k++) {
+			
+			int x = dx[k] + i;
+			int y = dy[k] + j;
+			
+			if(x >= 0 && y >= 0 && x < M && y < N) {
+				
+				if(cabbage[x][y] == 1) {
+					if(visit[x][y]) {
+						result = false;
+					}
+					else
+						visit[x][y] = true;
+				}
+			}
+		}	
+		
+		visit[i][j] = true;
+		
+		if(result)
+			bugs++;
+		
+	}
+}
