47dc633637eff11fd90c594606beaeec6dc84d03
realcommang <katlelo@naver.com>
2023-09-25 16:52:47 +0900

    [UPDATE] 4963

--- solution/src/week10/q4963.java
+++ solution/src/week10/q4963.java
@@ -5,26 +5,28 @@ import java.io.*;
 
 public class q4963 {
 
-	static int w=1;
-	static int h=1;
+	static int w, h;
 	static int count=0;
 	
-	static boolean [][] visit = new boolean [51][51]; //방문여부
-	static int [][] rec = new int [51][51]; //정사각형
+	static boolean [][] visit; //방문여부
+	static int [][] rec; //정사각형
 	
-	static int dx[] = {0,0,1,-1,1,1,-1,-1};
-	static int dy[] = {1,-1,0,0,1,-1,1,-1};
+	static int dx[] = {0, 0, -1 ,1, -1, 1, -1, 1};
+	static int dy[] = {-1, 1, 0, 0, 1, 1, -1, -1};
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-		while(w != 0 && h != 0) {
+		while(true) {
 			
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
 			w = Integer.parseInt(st.nextToken());
 			h = Integer.parseInt(st.nextToken());
+			
+			if(w == 0 && h == 0)
+				break;
 			
 			//System.out.println(w + " " + h);
 			rec = new int [h][w];
@@ -43,37 +45,39 @@ public class q4963 {
 				for(int j = 0; j < w; j++) {
 					if(rec[i][j] == 1 && !visit[i][j]) {
 						Island(i, j);
+						count++;
 					}
 				}
 			}
 			
-			System.out.println(count);	
-			//System.out.println();
+			System.out.println(count);
 		}
 		
 	}
 	
 	public static void Island(int i, int j) {
 		
-		boolean result = true;
 		visit[i][j] = true;
 		
-		for(int k = 0; k < 8; k++) {
-			int x = dx[k] + i;
-			int y = dy[k] + j;
+		Queue<int[]> que = new LinkedList<>();
+		que.offer(new int[] {i, j});
+		
+		while( !que.isEmpty() ) {
+			int[] pos = que.poll();
 			
-			if(x >= 0 && x < h && y < w && y >= 0) {
-				if(rec[x][y] == 1 && !visit[x][y]) {
-					visit[x][y] = true;
-				}
-				else if(rec[x][y] == 1 && visit[x][y]){
-					result = false;
+			for(int k = 0; k < 8; k++) {
+				
+				int nx = dx[k] + pos[0];
+				int ny = dy[k] + pos[1];
+				
+				if(nx >= 0 && ny >= 0 && nx < h && ny < w) {
+					if(!visit[nx][ny] && rec[nx][ny] == 1) {
+						visit[nx][ny] = true;
+						que.offer(new int[] {nx, ny});
+					}
 				}
 			}
 		}
-		
-		if(result)
-			count++;
 	}
 
 }
