18a8d5f92021f73bb587a25d9f4274f5ba6d4438
realcommang <katlelo@naver.com>
2023-10-02 11:41:55 +0900

    [UPDATE] 18232

--- solution/src/week10/q18232.java
+++ solution/src/week10/q18232.java
@@ -8,7 +8,8 @@ public class q18232 {
 	static int min = Integer.MAX_VALUE;
 	static int N, M, S, E;
 	//텔레포트 정보
-	static ArrayList<int []> teleport = new ArrayList<int []>();
+	static ArrayList<ArrayList<Integer>> teleport;
+	static boolean[] visited;
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -23,43 +24,67 @@ public class q18232 {
 		S = Integer.parseInt(st.nextToken()); //주예 위치
 		E = Integer.parseInt(st.nextToken()); //방주 위치
 		
+		visited = new boolean[N+1];
+		teleport = new ArrayList<>();
+		
+		for(int i = 0; i <= N; i++) {
+			teleport.add(new ArrayList<>());
+		}
+		
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			int [] tele = new int [2];
-			tele[0] = x; tele[1] = y;
-			teleport.add(tele);
+			
+			//양방향 연결
+			teleport.get(x).add(y);
+			teleport.get(y).add(x);
 		}
 		
-		int i = 0;
-		
-		while(i < teleport.size()) {
-			
-			if(teleport.get(i) == null) {
-				System.out.println("null");
-				break;
-			}
-			
-			int start = teleport.get(i)[0];
-			int end = teleport.get(i)[1];
-			
-			getMin(start, end);
-			i++;
-		}
-		
-		System.out.println(min);
+		int result = bfs();
+		System.out.println(result);		
 	}
-
-	static void getMin(int start, int end) {
+	
+	static int bfs() {
 		
-		int time = 1;
-		time += Math.abs(start - S);
-		time += Math.abs(end - E);
+		Queue<Integer> que = new LinkedList<>();
+		que.add(S);
+		visited[S] = true;
+		int time = 0;
 		
-		
-		
-		if(min > time)
-			min = time;
+		while(!que.isEmpty()) {
+			
+			int size = que.size();
+			
+			for(int i = 0; i < size; i++) {
+				int now = que.poll();
+				
+				if(now == E) {
+					return time;
+				}
+				
+				for(int next : teleport.get(now)) {
+					if(!visited[next]) {
+						que.add(next);
+						visited[next] = true;
+					}
+				}
+				
+				int next1 = now + 1;
+				int next2 = now -1;
+				
+				if(next1 <= N && !visited[next1]) {
+					que.add(next1);
+					visited[next1] = true;
+				}
+				
+				if(next2 >= 1 && !visited[next2]) {
+					que.add(next2);
+					visited[next2] = true;
+				}
+			}
+			time++;
+		}
+		return -1;
 	}
 }
