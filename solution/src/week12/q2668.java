b5fbf00282e38f1b7505212464c88cc7cb9cb854
realcommang <katlelo@naver.com>
2023-10-10 12:55:06 +0900

    [UPDATE] 2668

--- solution/src/week12/q2668.java
+++ solution/src/week12/q2668.java
@@ -8,69 +8,54 @@ import week12.q7569.Node;
 public class q2668 {
 
 	static int N;
-	static int [][] nums;
-	
+	static int[][] nums;
+
 	static ArrayList<Integer> arr = new ArrayList<>();
-	
-	public static class Node {
-		int start;
-		int end;
-		
-		public Node(int start) {
-			this.start = start;
-		}
-		
-		public Node(int start, int end) {
-			this.start = start;
-			this.end = end;
-		}
-	}
-	
-	static ArrayList<Node> arrays = new ArrayList<>();
-	
+	static boolean[] visit;
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
+
 		N = Integer.parseInt(br.readLine());
-		nums = new int [2][N+1];
+		nums = new int[2][N + 1];
+		visit = new boolean[N+1];
 		
-		for(int i = 1; i <= N; i++) { //첫째줄
-			nums[0][i] = i; 			
+		for (int i = 1; i <= N; i++) { // 첫째줄
+			nums[0][i] = i;
 		}
-		
-		arrays.add(new Node(0,0));
-		
-		for(int j = 1; j <= N; j++) { //둘째줄
+
+		for (int j = 1; j <= N; j++) { // 둘째줄
 			st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			nums[1][j] = start;
-			arrays.add(new Node(start, j));
+		}
+
+		for (int j = 1; j <= N; j++) { 
+			visit[j] = true;
+			dfs(j, j);
+			visit[j] = false;
 		}
 		
-		System.out.println(count());
 		Collections.sort(arr);
-		
-		for(int i = 0; i < arr.size(); i++) {
+		System.out.println(arr.size());
+		for (int i = 0; i < arr.size(); i++) {
 			System.out.println(arr.get(i));
 		}
-		
+
 	}
-	
-	public static int count() {
+
+	public static void dfs(int start, int target) {
 		
-		int count = 0;
-	
-		for(int i = 1; i <= N; i++) {
-			int end = nums[1][i];
-			Node node = arrays.get(end);
-			if((i == node.start) && (end == node.end)) {
-				count++;
-				arr.add(node.start);
-			}
+		if(!visit[nums[1][start]]) {
+			visit[nums[1][start]] = true;
+			dfs(nums[1][start], target);
+			visit[nums[1][start]] = false;
 		}
 		
-		return count;
+		if(nums[1][start] == target) {
+			arr.add(target);
+		}
 	}
 }
