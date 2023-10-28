3b23955fa7700457d4122f9322a15fc9c2a9da6b
realcommang <katlelo@naver.com>
2023-10-28 18:12:49 +0900

    [ADD] 15657

---
+++ solution/src/week14/q15657.java
@@ -0,0 +1,50 @@
+package week14;
+
+import java.util.*;
+import java.io.*;
+
+public class q15657 {
+	
+	static int N, M;
+	static int [] array;
+	static int [] NS; 
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		NS = new int[N];
+		
+		array = new int [M];
+		
+		for (int i = 0; i < N; i++) {
+			NS[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(NS);
+		
+		dfs(0, 0);
+		
+		bw.flush();
+		bw.close();
+	}
+
+	static void dfs(int start, int index) throws IOException{
+		
+		if(index == M) {
+			for (int i = 0; i < M; i++) {
+				bw.write(array[i] + " ");
+			}
+			bw.newLine();
+			return;
+		}
+		
+		for(int i = start; i < N; i++) {
+			array[index] = NS[i];
+			dfs(i, index + 1);
+		}		
+	}
+}
