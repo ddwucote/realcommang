4bea7f1e2c6894e413072863b481acf5d19cea01
realcommang <katlelo@naver.com>
2023-10-29 17:55:20 +0900

    [ADD] 올바른괄호

---
+++ solution/src/week14/올바른괄호.java
@@ -0,0 +1,36 @@
+package week14;
+
+import java.util.*;
+import java.io.*;
+
+public class 올바른괄호 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		
+		int num = 0;
+		
+		for(int i = 0; i < s.length(); i++) {
+			
+			if(num < 0) break;
+			
+			if(s.charAt(i) == '(') {
+				num++;
+			}
+			else if (s.charAt(i) == ')') {
+				num--;
+			}
+		}
+		
+		if(num == 0)
+			System.out.println("true");
+		else
+			System.out.println("false");
+		
+		
+	}
+
+}
