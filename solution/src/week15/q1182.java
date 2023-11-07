e9ba5cb9d35947f009d165a343b0a44f493d0d85
realcommang <katlelo@naver.com>
2023-11-07 22:43:13 +0900

    [UPDATE] 1182

--- solution/src/week15/q1182.java
+++ solution/src/week15/q1182.java
@@ -8,10 +8,11 @@ public class q1182 {
 	static int N, S;
 	static ArrayList<Integer> array = new ArrayList<Integer>();
 	static int sum = 0;
+	static int temp = 0;
 	static boolean [] visited;
 	
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
@@ -24,31 +25,42 @@ public class q1182 {
 			array.add(Integer.parseInt(st.nextToken()));
 		}
 		
-		for(int i = 0; i < N; i++) {
-			dfs(0,i, 0);
+		for(int i = 1; i < N; i++) {
+			
+			System.out.println("--------------------");
+			dfs(0, i, 0);
+			
 		}
 		
 		System.out.println(sum);
 	}
 
-	//start : 시작, index : 부분 수열 크기, count : 지금까지 합 
-	static void dfs(int start, int index, int count) {
-		
-		if(index == start) {
-			count = 0;
-			if(count == 0) {
+	//index : 부분 수열 크기, count : 지금까지 반복한 횟수 
+	static void dfs(int start, int index, int count) {		
+		if(index == count) {
+			
+			for(int i = start; i < N; i++) {
+				visited[i] = false;		
+			}
+			
+			if(temp == 0) 
 				sum++;
-			}			
+			
+			System.out.print(temp);
+			System.out.println();
+			
+			
+			count = 0; temp = 0;
 			return;
 		}
 		
 		for(int i = start; i < N; i++) {
 			if(!visited[i]) {
+				System.out.print(array.get(i) + " ");
+				temp += array.get(i);
 				visited[i] = true;
-				dfs(start, index+1, count+array.get(i));
-				
-			}
+				dfs(start, index, count+1);						
+			}			
 		}
-		
 	}
 }
