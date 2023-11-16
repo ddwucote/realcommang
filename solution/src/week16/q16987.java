ef9e5cd4dc79054db6b70bb721c11b5f13db0a7b
realcommang <katlelo@naver.com>
2023-11-16 13:55:14 +0900

    [UPDATE] 16987

--- solution/src/week16/q16987.java
+++ solution/src/week16/q16987.java
@@ -1,74 +1,85 @@
 package week16;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class q16987 {
 
-	static int N;
-	static ArrayList<List> eggs = new ArrayList<>();
-	static int max = 0;
-	
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		N = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			List<Integer> egg = new ArrayList<>();
-			egg.add(Integer.parseInt(st.nextToken()));
-			egg.add(Integer.parseInt(st.nextToken()));
-			eggs.add(egg);
-		}
-		
-		dfs(0, 0, 0);
-		
-		System.out.println(max);
-	}
-	
-	static void dfs(int index, int start, int sum) {
-		
-		if(index == N) {	
-			max = Math.max(sum, max);		
-			return;
-		}
-		
-		for(int i = start+1; i < N; i++) {
-			
-			if(Integer.parseInt(eggs.get(i).get(0).toString()) > 0) { 
-				
-				if(Integer.parseInt(eggs.get(start).get(0).toString()) > 0) {
-					
-					int left = Integer.parseInt(eggs.get(start).get(0).toString()) - 
-							Integer.parseInt(eggs.get(i).get(1).toString());
-					int right = Integer.parseInt(eggs.get(i).get(0).toString()) - 
-							Integer.parseInt(eggs.get(start).get(1).toString());
-					
-					System.out.println("left: " + left + " right: " + right);
-					
-					List<Integer> eggL = new ArrayList<>();
-					eggL.add(left); eggL.add(Integer.parseInt(eggs.get(start).get(1).toString()));
-					eggs.set(start, eggL);
-					
-					
-					List<Integer> eggR = new ArrayList<>();
-					eggR.add(right); eggR.add(Integer.parseInt(eggs.get(i).get(1).toString()));
-					eggs.set(i, eggR);
-					
-					if(left < 0 && right < 0)
-						dfs(index+2, start+1, sum+2);
-					else if (left > 0 && right < 0)
-						dfs(index+1, start+1, sum+1);
-					else if (left < 0 && right > 0)
-						dfs(index+1, start+1, sum+1);
-				}
-				
-				dfs(index+1, i, sum);
-			}
-		}
-	}
+    static int N;
+    static ArrayList<List<Integer>> eggs = new ArrayList<>();
+    static int max = 0;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            List<Integer> egg = new ArrayList<>();
+            egg.add(Integer.parseInt(st.nextToken()));
+            egg.add(Integer.parseInt(st.nextToken()));
+            eggs.add(egg);
+        }
+
+        dfs(0);
+
+        System.out.println(max);
+    }
+
+    static void dfs(int index) {
+        if (index == N) {
+            int count = 0;
+            for (List<Integer> egg : eggs) {
+                if (egg.get(0) <= 0) {
+                    count++;
+                }
+            }
+            max = Math.max(max, count);
+            return;
+        }
+
+        if (eggs.get(index).get(0) <= 0) {
+            dfs(index + 1);
+            return;
+        }
+
+        boolean canBreak = false;
+
+        for (int i = 0; i < N; i++) {
+            if (i != index && eggs.get(i).get(0) > 0) {
+                canBreak = true;
+
+                List<Integer> originalEggL = new ArrayList<>(eggs.get(index));
+                List<Integer> originalEggR = new ArrayList<>(eggs.get(i));
+
+                List<Integer> eggL = new ArrayList<>(eggs.get(index));
+                List<Integer> eggR = new ArrayList<>(eggs.get(i));
+
+                int left = eggL.get(0) - eggR.get(1);
+                int right = eggR.get(0) - eggL.get(1);
+
+                eggL.set(0, left);
+                eggs.set(index, eggL);
+
+                eggR.set(0, right);
+                eggs.set(i, eggR);
+
+                dfs(index + 1);
+
+                // 백트래킹
+                eggs.set(index, originalEggL);
+                eggs.set(i, originalEggR);
+            }
+        }
+
+        if (!canBreak) {
+            dfs(index + 1);
+        }
+    }
 }
