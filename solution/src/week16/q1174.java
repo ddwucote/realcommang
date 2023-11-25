66ee043e691a17b390681d8b8d176c177c1ade99
realcommang <katlelo@naver.com>
2023-11-25 13:01:55 +0900

    [ADD] 1174

--- solution/src/week16/q1174.java
+++ solution/src/week16/q1174.java
@@ -7,50 +7,51 @@ public class q1174 {
 
 	static long N;
 	static long start = 0;
+	static String s;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
 		N = sc.nextLong();
-		dfs(1);
+	
+		while(N > 0) {
+			
+			int i = 0, j = 0;
+			s = Long.toString(start);
+			char max = s.charAt(0);
+			
+			while(i < s.length()-1) {
+				
+				//System.out.println("prev : " + s.charAt(i) + " next : " + s.charAt(i+1));
+				
+				if((s.charAt(i) - '0') <= (s.charAt(i+1) - '0'))
+					j++;
+				
+				if(i >= 1 && ((s.charAt(i) - '0') == (max - '0') || (s.charAt(i) - '0') > (max - '0'))) {
+					j++;
+					max = s.charAt(i);
+				}
+				
+				i++;				
+			}
+			
+			if(j == 0) {
+				//System.out.println(start);
+				N -= 1;
+			}
+			
+			if(N <= 0)
+				break;
+			
+			start++;
+			
+			if(start > 1000000) {
+				start = -1;
+				break;
+			}			
+		}
+		
 		System.out.println(start);
-	}
-
-	static void dfs(long count) {
-		
-		if(start > 1000000) {
-			start = -1;
-			return;
-		}
-		
-		if(count == N) {
-			return;
-		}
-		
-		long temp = start;
-		long i = 0, j = 0;
-		long prev;
-		
-		prev = temp / 10;
-		
-		while(temp >= 10) {
-			
-			if(prev > temp % 10)
-				i++;			
-			j++; 
-			
-			prev = temp % 10;
-			temp = prev / 10;
-		}
-		
-		start++;
-		
-		if(i == j) {			
-			dfs(count+1);
-		}
-		else {
-			dfs(count);
-		}
 	}
 }
