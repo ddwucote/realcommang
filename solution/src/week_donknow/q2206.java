2790e45613f0eb6072069cc9e04b43c5e69d5293
realcommang <katlelo@naver.com>
2024-01-03 17:50:18 +0900

    [ADD] 2206

--- solution/src/week_donknow/q2206.java
+++ solution/src/week_donknow/q2206.java
@@ -19,8 +19,7 @@ public class q2206 {
 
 	static int N, M;
 	static int [][] map;
-	static boolean [][] visited;
-	static int min = Integer.MAX_VALUE;
+	static boolean [][][] visited;
 	
 	static int dx[] = {-1, 0, 1, 0};
     static int dy[] = {0, 1, 0, -1};
@@ -34,7 +33,7 @@ public class q2206 {
 		M = Integer.parseInt(st.nextToken());
 		
 		map = new int [N][M];
-		visited = new boolean [N][M];
+		visited = new boolean [N][M][2];
 		
 		for(int i = 0; i < N; i++) {
 			String line = br.readLine();
@@ -44,41 +43,45 @@ public class q2206 {
 		}
 		
 		bfs(0, 0);
-		
-		System.out.println(min);
 	}
 	
 	static void bfs(int x, int y) {
 		
         Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(x, y, 1, false));
-        
-        visited[x][y] = true;
+        queue.add(new Node(0, 0, 1, false));
         
         while (!queue.isEmpty()) {
             Node node = queue.poll();
             
             if (node.x == N - 1 && node.y == M - 1) {
-                min = Math.min(node.distance, min);
-                break;
+                System.out.println(node.distance);
+                return;
             }
 
-            for (int i = 0; i < 4; i++) {
-               	
+            for (int i = 0; i < 4; i++) {               	
                 int j = node.x + dx[i];
                 int k = node.y + dy[i];
 
                 if (j >= 0 && k >= 0 && j < N && k < M) {
-                    if (map[j][k] == 0 && !visited[j][k]) {
-                        visited[j][k] = true;
-                        queue.add(new Node(j, k, node.distance + 1, node.wall));
-                    } else if (map[j][k] == 1 && node.wall == false && !visited[j][k]) {
-                        visited[j][k] = true;
-                        queue.add(new Node(j, k, node.distance + 1, true));
+                	
+                    if(map[j][k] == 0) {
+                    	if(!node.wall && !visited[j][k][0]) {
+                    		queue.add(new Node(j, k, node.distance+1, false));
+                    		visited[j][k][0] = true;
+                    	} else if(node.wall && !visited[j][k][1]) {
+                    		queue.add(new Node(j, k, node.distance+1, true));
+                    		visited[j][k][1] = true;
+                    	}
+                    }else if(map[j][k] == 1) {
+                    	if(!node.wall) {
+                    		queue.add(new Node(j, k, node.distance+1, true));
+                    		visited[j][k][1] = true;
+                    	}
                     }
                 }
             }
         }
+        System.out.println(-1);
 	}
 }
 
